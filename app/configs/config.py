40356d0f285b1fcf26fde52e1d63414cf670c6cf
Emilianissimo <emilerofeevskij@gmail.com>
2023-03-16 14:12:46 +0500

    feat: added jwtservice

--- app/configs/config.py
+++ app/configs/config.py
@@ -3,6 +3,8 @@ from pydantic import BaseSettings, Field
 
 class Settings(BaseSettings):
     db_url: str = Field(..., env='DATABASE_URL')
+    secret_key: str = Field(..., env='SECRET_KEY')
+    algorithm: str = Field(..., env='ALGORITHM')
 
 
 settings = Settings()
--- app/services/jwt_service.py
+++ app/services/jwt_service.py
@@ -1,12 +1,13 @@
 import jwt
 from app.models.user import User
+from app.configs.config import settings
 
 
 class JWTService:
     def __init__(self, user: User):
         self.user = user
 
-    def create_token_by_user(self, additional_payload={}):
+    def create_token_by_user(self, additional_payload: dict={})-> str:
         payload = {
             'sub': self.user.id,
             'email': self.user.email,
@@ -15,3 +16,9 @@ class JWTService:
             'home_address': self.user.home_address,
         }
         payload += additional_payload
+        token = jwt.encode(payload, settings.secret_key, algorithm=settings.algorithm)
+        return token
+
+    @staticmethod
+    def get_payload_from_token(token: str)-> dict:
+        return jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm])
